de987ac5aae3714fe0773ed82b58999cd288a03d
Fer Martin <fernandoMartinSantana@gmail.com>
2020-03-18 11:34:43 +0000

    remove todos

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,13 +2,11 @@ const router = require('express').Router()
 
 const usersRouter = require('./users.router')
 const authRouter = require('./auth.router')
-const todosRouter = require('./todos.router')
 const teacherRouter = require('./teachers.router')
 const { authUser } = require('../utils') // Authenticated Route
 
 router.use('/users', usersRouter)
 router.use('/auth', authRouter)
-router.use('/todos', todosRouter)
 router.use('/teachers', teacherRouter)
 
 router.get('/whoami', authUser, (req, res) => {
