55b5bda905655bc7ab80b056696e30e656c0eba7
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-03-18 19:26:46 +0000

    Merge

--- api/routes/lessons.router.js
+++ api/routes/lessons.router.js
@@ -14,13 +14,4 @@ router.get('/:id', getOneLesson)
 router.put('/:id', updateLesson)
 router.delete('/:id', deleteLesson)
 
-<<<<<<< HEAD
-router.get('/me', authUser, getMyLessons)
-router.post('/me', authUser, addMyLesson)
-router.get('/me/:id', authUser, getMyLesson)
-router.put('/me/:id', authUser, updateMyLesson)
-router.delete('/me/:id', authUser, deleteMyLesson)
-
-=======
->>>>>>> 4bf91b5bfeeed2c6e6be6a1417ea8140571bcb4e
 module.exports = router
