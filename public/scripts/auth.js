6f7228918345047b6d36b0377b1de609a40b9886
Ollebacx <dav.ollebac@gmail.com>
2020-03-19 15:24:51 +0000

    signup finished

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -6,9 +6,10 @@ const api = axios.create({
 
 document.getElementById('btn-signup').addEventListener('click', (event) => {
   const newUser = {
-    user_name: document.getElementById('user_name').value,
-    user_email: document.getElementById('user_email').value,
-    user_password: document.getElementById('user_password').value
+    firstName: document.getElementById('signup_firstName').value,
+    lastName: document.getElementById('signup_lastName').value,
+    email: document.getElementById('signup_email').value,
+    password: document.getElementById('signup_password').value
   }
 
   api
@@ -18,10 +19,11 @@ document.getElementById('btn-signup').addEventListener('click', (event) => {
     // crea el token y me lo devuelve
     //
     .then(function (response) {
+      localStorage.clear()
       localStorage.setItem('token', response.data.token)
-      localStorage.setItem('name', response.data.username)
       localStorage.setItem('email', response.data.email)
-      location.assign('todos.html')
+      localStorage.setItem('firstName', response.data.firstName)
+      location.assign('explore.lessons.html')
     })
     .catch(function (error) {
       console.log(error.response)
