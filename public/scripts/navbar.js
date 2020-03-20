1fac22b6d03eb4843692fb20e3e413b0f992f0a5
Ollebacx <dav.ollebac@gmail.com>
2020-03-20 06:25:09 +0000

    Vertical navbar

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,6 +1,19 @@
 $(function () {
   // Sidebar toggle behavior
   $('#sidebarCollapse').on('click', function () {
-    $('#sidebar, #content').toggleClass('active')
+    $('#sidebar, #content, #name, #explore, #lessons, #logout, img').toggleClass('active')
   })
 })
+/* $(function () {
+  // Sidebar toggle behavior
+  $('#sidebarCollapse').on('click', function () {
+    $('#name, #explore, #lessons, #logout').toggleClass('active')
+  })
+})
+document.getElementById('sidebarCollapse').addEventListener('click', function (event) {
+  const hidden = document.querySelectorAll('.menu-text')
+  hidden.forEach(elem => {
+  elem.classList.toggle('hidden-text')
+  })
+document.getElementsByClassName('sidebar')[0].classList.toggle('sidebar-collapsed')
+}) */
